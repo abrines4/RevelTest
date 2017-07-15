f0653efba7e9eeace23ee8482f6b92f90e4d0f68
Brad Swain <bradleyswain1@gmail.com>
2017-07-15 16:32:54 -0500

    swap sqlite for mysql
    password game on point

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ GOPATH=/home/vagrant/go
 
 apt-get update
 
-apt-get -y install git mercurial sqlite3 libsqlite3-dev
+apt-get -y install git mercurial
 
 if [ ! -d $GO_INSTALL_FOLDER/go ];then
 	echo 'Downloading go$GO_VERSION.linux-amd64.tar.gz'
@@ -27,8 +27,16 @@ else
 	echo 'Go already installed. Skipping.'
 fi
 
+
+echo 'Getting Go packages'
 export PATH=$PATH:$GO_INSTALL_FOLDER/go/bin
 export GOPATH=$GOPATH
 
 go get -v github.com/revel/cmd/revel
 go get -v github.com/go-gorp/gorp
+
+echo 'Installing Database'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password testpassword'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password testpassword'
+apt-get update
+apt-get install -y mysql-server

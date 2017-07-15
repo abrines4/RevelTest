6cd3b5f55ff4de420c4999140c9ec50256b9a936
Brad Swain <bradleyswain1@gmail.com>
2017-07-15 17:52:34 -0500

    create booking table in vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,13 @@ GO_INSTALL_FOLDER=/usr/local
 
 GOPATH=/home/vagrant/go
 
+
 apt-get update
 
 apt-get -y install git mercurial
 
 if [ ! -d $GO_INSTALL_FOLDER/go ];then
-	echo 'Downloading go$GO_VERSION.linux-amd64.tar.gz'
+	echo 'Downloading go'$GO_VERSION'.linux-amd64.tar.gz'
 	wget –q https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
 
 	echo 'Unpacking go language'
@@ -34,9 +35,19 @@ export GOPATH=$GOPATH
 
 go get -v github.com/revel/cmd/revel
 go get -v github.com/go-gorp/gorp
+go get -v github.com/go-sql-driver/mysql
+
 
 echo 'Installing Database'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password testpassword'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password testpassword'
-apt-get update
 apt-get install -y mysql-server
+
+# From http://www.websightdesigns.com/posts/view/how-to-configure-an-ubuntu-web-server-vm-with-vagrant
+if [ ! -f /var/lof/dbinstalled ];then
+	echo "CREATE USER 'booker'@'localhost' IDENTIFIED BY 'testpassword'" | mysql -uroot -ptestpassword
+	echo "CREATE DATABASE booking" | mysql -uroot -ptestpassword
+	echo "GRANT ALL ON booking.* TO 'booker'@'localhost'" | mysql -uroot -ptestpassword
+	echo "flush privileges" | mysql -uroot -ptestpassword
+	touch /var/log/dbinstalled
+fi
